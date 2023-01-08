bb5388a247aca579e31e2b24f63025ba8d6f9391
Nita1001 <devinedevops@gmail.com>
2023-01-08 22:07:58 +0200

    exercise 1

--- exercise1/primitiveDataTepe.js
+++ exercise1/primitiveDataTepe.js
@@ -1,1 +1,26 @@
-//something
+
+
+
+//String 
+let myName ="Nita";
+//Number 
+let myAge = 29;
+//Object 
+const me = {myName, myAge};
+//Boolean 
+let cssIsFun = true;
+//Null
+let someThing = null;
+//Undefined
+let thisIsUndefined;
+//Symbol
+let questionMark = '?';
+
+
+console.log(typeof(myName));
+console.log(typeof(myAge));
+console.log(typeof(cssIsFun));
+console.log(typeof(someThing));
+console.log(typeof(thisIsUndefined));
+console.log(typeof(questionMark));
+
