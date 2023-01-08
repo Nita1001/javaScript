ea2f7e8c422f8b36e8c81d5abe80f90e4e2a4ed8
Nita1001 <devinedevops@gmail.com>
2023-01-09 00:36:22 +0200

    exercise 1.2

---
+++ exercize2/ex2.1.js
@@ -0,0 +1,15 @@
+// a function called countryInfo
+
+let sentence ;
+function countryInfo (country, population, capitalCity) 
+{
+  country = "Spain" ;
+  population = 47;
+  capitalCity = "Madrid";
+
+  console.log(country + ' ' + "has a" + " " + population + " million people and its capital city is" + capitalCity);
+} 
+
+
+countryInfo();
+
