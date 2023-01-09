c37f319c2cb68c75184e1822b0d96b5b1b874d61
Nita1001 <devinedevops@gmail.com>
2023-01-09 08:48:15 +0200

    added ex Instructions

--- exercise1/primitiveDataType.js
+++ exercise1/primitiveDataType.js
@@ -1,5 +1,13 @@
 
-
+/*
+1. Create a variable whose value holds a string.
+2. Create a variable whose value holds a number.
+3. Create a variable whose value holds an object.
+4. Create a variable whose value holds a boolean.
+5. Create a variable whose value holds null.
+6. Create a variable whose value is undefined.
+7. Create a variable whose value holds a symbol.
+*/
 
 //String 
 let myName ="Nita";
--- exercise2/ex2.1.js
+++ exercise2/ex2.1.js
@@ -1,3 +1,13 @@
+/*
+1. Write a function called ‘countryInfo’ which takes three
+parameters: 'country', 'population' and 'capitalCity'. Based
+on this input, the function returns a string with this format:
+‘Spain has 47 million people and its capital city is Madrid’
+2. Call this function 3 times, with input data for 3 different
+countries. Store the returned values in 3 different
+variables, and log them to the console.
+*/
+
 // a function called countryInfo
 let sentence ;
 const countryInfo  = (country, population, capitalCity) =>
