9b683939f59142fbc85ceb9a010d34c9fc474ba1
Nita1001 <devinedevops@gmail.com>
2023-01-09 08:27:43 +0200

    ex 2.3

---
+++ exercise2/ex2.3.js
@@ -0,0 +1,14 @@
+/* 
+Write a function that calculates the circle area by a given radius
+as an argument. Print the area as it is calculated and then print
+it rounded to two decimal places.
+*/
+
+const calcCircleArea = (radius) => {
+  const pi = 3.14159;
+  let circleArea = 2 * pi * radius;
+  console.log(circleArea);
+  console.log(Math.round(circleArea));
+}
+
+calcCircleArea(3);
