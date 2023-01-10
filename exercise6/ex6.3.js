7f74b0a32829222ec3e0bc8e7ea1913d9f5d6776
Nita1001 <devinedevops@gmail.com>
2023-01-10 15:46:23 +0200

    Initial commit

---
+++ exercise6/ex6.3.js
@@ -0,0 +1,6 @@
+// Write a function that will receive an array of strings and will
+// return a new array containing all the lengths of all the strings in
+// the array it got as a parameter.
+// So that if you call this function with this array for example:
+// ["boo", "doooo", "hoo","ro"]
+// it will return this array: [3, 5, 3, 2].
