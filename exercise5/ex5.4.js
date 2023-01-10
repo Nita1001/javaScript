410d14aa27d9a53d321da8f0af95bea857f31cc9
Nita1001 <devinedevops@gmail.com>
2023-01-10 13:51:29 +0200

    Initial commit

---
+++ exercise5/ex5.4.js
@@ -0,0 +1,11 @@
+/*
+Create a function that receives one argument, a string color.
+Create a switch statement that:
+1. If the color is yellow, pink, or orange return from the
+function “light color”.
+2. If the color is a blue, purple, or brown return from the
+function “dark color”.
+3. If the color is none of the above return “Unknown color”.
+Note:
+It shouldn’t be case sensitive */
+
