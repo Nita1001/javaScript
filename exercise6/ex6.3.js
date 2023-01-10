44fd490b8dd4009c96c1380a92d09e0767499695
Nita1001 <devinedevops@gmail.com>
2023-01-10 20:14:35 +0200

    done

--- exercise6/ex6.3.js
+++ exercise6/ex6.3.js
@@ -4,3 +4,13 @@
 // So that if you call this function with this array for example:
 // ["boo", "doooo", "hoo","ro"]
 // it will return this array: [3, 5, 3, 2].
+const lengthOfStrs = (str) =>{
+  let newStr = [];
+  for(let i = 0; i < str.length; i++){
+    newStr[i] = str[i].length;
+  }
+  return newStr;
+}
+let str = ["boo", "doooo", "hoo","ro"];
+let newStr = lengthOfStrs(str);
+console.log(newStr);
