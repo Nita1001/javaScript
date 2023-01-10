bbaaa09d292a9733f83d919f56e39a650261ff6f
Nita1001 <devinedevops@gmail.com>
2023-01-10 10:53:08 +0200

    done

--- exercise4/ex4.4.js
+++ exercise4/ex4.4.js
@@ -9,19 +9,17 @@ Example of numbers between 1 to 18:
 1,2,3,4,5,6, BOOM-BOOM,8,9,10,11,12,13,BOOM,15,16,17,18
 */
 
-const printNum = num => {
-  if(num == 0)
-  {
-    return;
-  }
-  if(num % 7 == 0){
-    console.log("BOOM");
-  }
-  else{ 
-    console.log(num);
-  }
-  return printNum(num - 1);
-}
-let num = 10;
+const printNum = (num) => {
+    if(num === 18){ // Stop condition
+      return;
+    } else if (num % 7 === 0 && num.toString().includes("7")) {
+      console.log("BOOM-BOOM");
+    } else if (num % 7 === 0) {
+      console.log("BOOM");
+    } else {
+      console.log(num);
+    }
+    return printNum(num + 1);
+};
+let num = 1;
 printNum(num);
-
