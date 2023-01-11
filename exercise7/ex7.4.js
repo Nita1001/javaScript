204f0abb3ae5cb0ef7a72cd43e69417553a3477b
Nita1001 <devinedevops@gmail.com>
2023-01-11 15:09:53 +0200

    done

---
+++ exercise7/ex7.4.js
@@ -0,0 +1,6 @@
+// You have this array:
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// using splice modify the array to be this:
+// ["Banana", "Orange", "Lemon", "Kiwi", "Apple","Mango"];
+fruits.splice(2, 0, "Lemon", "Kiwi");
+console.log(fruits);
