9e83620983051bea50470e775e9189dc359aff80
Nita1001 <devinedevops@gmail.com>
2023-01-11 13:39:53 +0200

    done

---
+++ exercise7/ex7.2.js
@@ -0,0 +1,22 @@
+// you have this string:
+const string = "The more you know, the more you know that you don't know";
+// We want to get the index of the last occurrence of the word
+// “you”. It should give you 42.
+console.log(string.lastIndexOf("you"));
+// You can solve this problem without the method indexOf. But
+// if you can manage to do it also with indexOf you will get a
+// cookie
+
+let lastWord = 'you';
+let lastIndex = -1;
+// i want lastIndex to jump 1 index at a time, start from index 0, until the word is no more to be found.  
+while(true){
+  let index = string.indexOf('you', lastIndex + 1);
+  if(index == -1){
+    break;
+  }
+  lastIndex = index;
+}
+console.log(lastIndex);
+
+/*Now just Waiting for my cookie :) */
