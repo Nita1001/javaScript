2462fc09408c282acb3908d3e400c740e0fc4d01
Nita1001 <devinedevops@gmail.com>
2023-01-11 13:18:48 +0200

    Initial commit

---
+++ exercise7/ex7.1.js
@@ -0,0 +1,15 @@
+// Create a function that received an array of integers as a
+// parameter.
+// Return a new array without any duplicates inside.
+// use indexOf as part of your solution.
+// Example:
+// [3,4,4,3,6,3] --→ [3,4,6]
+
+const noDuplicates = (array) =>{
+  let newArray = [];
+
+}
+
+const duplicatedArr = [3, 4, 4, 3, 6, 3];
+let newArr = noDuplicates(duplicatedArr);
+console.log(newArr);
