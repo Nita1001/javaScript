450f2f2129bdb835eea57313f2b6fef3d02eebb1
Nita1001 <devinedevops@gmail.com>
2023-01-15 14:37:56 +0200

    Initial commit

---
+++ exercise10/ex10.1.js
@@ -0,0 +1,14 @@
+// 1. Write a function called ‘isString’ that receives 2 arguments,
+// a string and a callback function. The function checks that
+// the string is indeed a string. If the string is a string, pass
+// the string to a callback function that logs that string to the
+// console.
+// 2. Create a function called ‘firstWordUpperCase’ that
+// receives 2 arguments, a string and a callback function.
+// The function will capitalize the first word in the sentence
+// and pass the string to a callback function which will create
+// dashes between the words.
+// 3. Call the ‘firstWordUpperCase’ function with a callback of
+// your choice.
+// 4. Create your own function that will receive from one of its
+// arguments a callback function.
