5dfda44c0631cbd7b78694644a367172cd4b1be3
Nita1001 <devinedevops@gmail.com>
2023-01-15 11:57:47 +0200

    done

---
+++ exercise9/ex9.3.js
@@ -0,0 +1,24 @@
+// You are given two arrays:
+const food = ["Noodle", "Pasta", "Ice-cream",
+"Meat", "Cucumber", "Olives"];
+const food1 = ["Fries", "Ice-cream", "Pizza",
+"Olives", "Hamburgers"];
+// Create a function that takes these two arrays as
+// arguments.
+// Compare these two arrays using 2 for loops and return the
+// items that are the same. If none are the same return false.
+
+const compare = (str1, str2) => {
+  let matchOrnot = [];
+  let length = str1.length >= str2.length ? str1.length : str2.length;
+  for(let i = 0; i < length; i++)
+  {
+    for(let j = 0; j < length; j++){
+      (str1[i] === str2[j])? matchOrnot.push(str2[j]): false;
+    }
+
+  }
+  return matchOrnot;
+}
+
+console.log(compare(food, food1));
