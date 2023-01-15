79cb9bb4ec44a6cfdcb40d8e84266afc2d4a2f50
Nita1001 <devinedevops@gmail.com>
2023-01-15 14:34:52 +0200

    halfway trough

--- exercise9/ex9.4.js
+++ exercise9/ex9.4.js
@@ -3,22 +3,19 @@
 // The function should console log a step shape.
 // With N levels using the # character. Make sure the step has
 // spaces on the right-hand side if empty!
-
 const print = (n) =>{
   let str = [];
   for(let col = 0; col < n ; col++){
     for(let row = 0; row < col; row++){
-      
+      str +='#'
     }
+    str +='\n';
   }
-
+  console.log(str);
 }
-
-
 let n = 5;
 (Math.sign(n) === -1) ? n *= -1 : false;
 print(n);
-
 // Note:
 // Examples:
 // steps(2)
