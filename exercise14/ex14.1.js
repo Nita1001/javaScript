768cd6f6390e036e91b0db3b278befba4725856e
Nita1001 <devinedevops@gmail.com>
2023-01-22 21:59:47 +0200

    prettier

--- exercise14/ex14.1.js
+++ exercise14/ex14.1.js
@@ -1,12 +1,12 @@
 // Write the following functions using the reduce built-in function.
 // 1. max
-const arr = [1,34, 78, 1, -3];
-const max = (arr) => arr.reduce((a,b) =>  a>b ? a: b);
+const arr = [1, 34, 78, 1, -3];
+const max = (arr) => arr.reduce((a, b) => (a > b ? a : b));
 console.log(max(arr));
 // 2. the sum of even numbers
-const arr2 = [3,3,3]
-const sum = (arr) => arr.reduce((a,b) => a+b );
+const arr2 = [3, 3, 3];
+const sum = (arr) => arr.reduce((a, b) => a + b);
 console.log(sum(arr2));
 // 3. average
-const avg = (arr) => arr.reduce((a,b) => sum(arr)/arr.length);
+const avg = (arr) => arr.reduce((a, b) => sum(arr) / arr.length);
 console.log(avg(arr2));
