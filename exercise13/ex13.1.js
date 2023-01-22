0e4debcd1e37319eea96d6e7371d96504178d457
Nita1001 <devinedevops@gmail.com>
2023-01-22 21:36:24 +0200

    done

--- exercise13/ex13.1.js
+++ exercise13/ex13.1.js
@@ -3,6 +3,9 @@
 const numbers = [1, -5, 666, 2, 400, 11];
 // 1. Sort the array of numbers from descending to ascending
 // order.
-
+numbers.sort(function(a, b){return a-b});
+console.log(numbers);
 // 2. Sort the array of numbers from ascending to descending
 // order.
+numbers.sort(function(a, b){return b-a});
+console.log(numbers);
