97724564d95a7544f14b841e9edd5d8097769d98
Nita1001 <devinedevops@gmail.com>
2023-01-22 22:23:14 +0200

    done

--- exercise15/ex15.3.js
+++ exercise15/ex15.3.js
@@ -1,15 +1,14 @@
 // # Note: We ask you not to solve the bug by finding it with
-// your
-// eyes but to use the debugging tools we've learned!
+// your eyes but to use the debugging tools we've learned!
 // What is wrong with this code?
 // 1. First, find the line that contains the problem. Write it down.
-// 2. Which debug method did you use to find the bug?
-// 3. Explain the bug in your own words.
+// 2. Which debug method did you use to find the bug? //?? my eyes :<
+// 3. Explain the bug in your own words. //?? indexes were incorrect
 // 4. Fix the code and submit it all.
 // we want to sum all numbers in even cells in arrays of size 10:
 // [1,2,3,4,5,6,7,8,9,10] => 2+4+6+8+10 = 30
-// function getSumOfEven(arr){
-// return arr[2] + arr[4] + arr[6] + arr[8] +
-// arr[10];
-// }
-// getSumOfEven([1,2,3,4,5,6,7,8,9,10]);
+function getSumOfEven(arr) {
+    let sum = 0;
+    return sum += arr[1] + arr[3] + arr[5] + arr[7] + arr[9];
+}
+console.log(getSumOfEven([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
