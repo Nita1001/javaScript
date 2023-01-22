78f4a9831a4b1d0cdf08b282114271b4311892a6
Nita1001 <devinedevops@gmail.com>
2023-01-22 22:14:57 +0200

    done

--- exercise15/ex15.2.js
+++ exercise15/ex15.2.js
@@ -1,17 +1,17 @@
 // # Note: We ask you not to solve the bug by finding it with
 // your eyes but to use the debugging tools we've learned!
 // What is wrong with this code?
-// 1. First, find the line that contains the problem. Write it down.
-// 2. Which debug method did you use to find the bug?
-// 3. Explain the bug in your own words.
+// 1. First, find the line that contains the problem. Write it down. //?? line 17, wrong function name. also line 11 & 12.
+// 2. Which debug method did you use to find the bug? //?? my eyes
+// 3. Explain the bug in your own words. //?? the function returned the largest num, because of wrong condition statements.
 // 4. Fix the code and submit it all.
-// function findSmallest(a, b, c){
-// if (a > b > c){
-// return c;
-// } else if (a > c > b) {
-// return a;
-// } else {
-// return b;
-// }
-// }
-// findSmalest(52,66, 2);
+function findSmallest(a, b, c) {
+    if (a < b && a < c) {
+        return c;
+    } else if (b < a &&  b < c) {
+        return b;
+    } else {
+        return c;
+    }
+}
+console.log(findSmallest(52, 66, 2));
