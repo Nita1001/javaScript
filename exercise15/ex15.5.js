68510cfac48a9f7f4755185e255e0beeeee00024
Nita1001 <devinedevops@gmail.com>
2023-01-22 22:32:07 +0200

    done

--- exercise15/ex15.5.js
+++ exercise15/ex15.5.js
@@ -1,16 +1,18 @@
 // # Note: We ask you not to solve the bug by finding it with
 // your eyes but to use the debugging tools we've learned!
 // What is wrong with this code?
-// 1. First, find the line that contains the problem. Write it down.
-// 2. Which debug method did you use to find the bug?
-// 3. Explain the bug in your own words.
+// 1. First, find the line that contains the problem. Write it down. //?? line 12 
+// 2. Which debug method did you use to find the bug? //?? REPL
+// 3. Explain the bug in your own words. //?? counters was incremented 3 times but the value wasn't stored each iteration.
 // 4. Fix the code and submit it all.
-// function countOccurrences (str, char){
-// let counter = 0 ;
-// for ( let i = 0 ; i < str .length; i ++ ){
-// if ( str . charAt ( i ) === char ){
-// counter + 1 ;
-// }
-// } return counter ;
-// }
-// countOccurrences ( "ini mini miny moe" , "n" );
+function countOccurrences(str, char) {
+    let counter = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (str.charAt(i) === char) {
+            counter += 1;
+        }
+    }
+    return counter;
+}
+
+console.log(countOccurrences("ini mini miny moe", "n"));
