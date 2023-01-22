b1d0a0bc7219bd452d76d15d3c2904acb1d5d486
Nita1001 <devinedevops@gmail.com>
2023-01-22 21:22:32 +0200

    Initial commit

---
+++ exercise17/ex17.1.js
@@ -0,0 +1,31 @@
+// Without running the code below, explain in your own
+// words what the result of each block of code will be and
+// why.
+// Block 1
+// var b = 1;
+// function someFunction(number) {
+// function otherFunction(input) {
+// return b;
+// }
+// b = 5;
+// return otherFunction;
+// }
+// var firstResult = someFunction(9);
+// var result = firstResult(2);
+// Block 2
+// var a = 1;
+// function b2() {
+// a = 10;
+// return;
+// function a() { }
+// }
+// b2();
+// console.log(a);
+// Block 3
+// let i;
+// for (i = 0; i < 3; i++) {
+// const log = () => {
+// console.log(i);
+// }
+// setTimeout(log, 100);
+// }
