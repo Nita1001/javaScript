d75aac09c953f290bf866cb78fb634a012817d9a
Nita1001 <devinedevops@gmail.com>
2023-01-22 22:27:30 +0200

    done

--- exercise15/ex15.4.js
+++ exercise15/ex15.4.js
@@ -1,14 +1,15 @@
 // # Note: We ask you not to solve the bug by finding it with
 // your eyes but to use the debugging tools we've learned!
 // What is wrong with this code?
-// 1. First find the line that contains the problem. Write it down.
-// 2. Which debug method did you use to find the bug?
-// 3. Explain the bug in your own words.
+// 1. First find the line that contains the problem. Write it down. //?? line 9.
+// 2. Which debug method did you use to find the bug? //?? my eyes :<
+// 3. Explain the bug in your own words. //?? sum wasn't initialized with a value of 0, or anything at all.
 // 4. Fix the code and submit it all.
-// function calcAverage (arr){
-// var sum ;
-// for ( var i = 0 ; i < arr .length; i ++ ){
-// sum += arr [ i ];
-// } return sum ;
-// }
-// calcAverage ([ 85 , 90 , 92 ]);
+function calcAverage(arr) {
+    var sum = 0;
+    for (var i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+console.log(calcAverage([85, 90, 92]));
