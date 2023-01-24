c15ff495bb0b988c9b40f7008bae5beba7aac117
Nita1001 <devinedevops@gmail.com>
2023-01-24 23:13:47 +0200

    Initial commit

---
+++ exercise19/ex19.3/ex19.3.js
@@ -0,0 +1,13 @@
+// Instructions
+// Many times an application needs to verify the account by
+// sending a verification code by text message.
+// Let's create functionality that enables the user to paste the
+// verification code to the input fields.
+// Features:
+// 1. The user is allowed to type the values manually. After
+// each value is inserted, the next input will be focused.
+
+// 2. The user is allowed to paste the verification code.
+
+// 3. An extra challenge will be to auto submit the form once all
+// inputs all populated.
