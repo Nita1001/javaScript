4d8a594d1ce7308e2d77700ac7eda65e6285758a
Nita1001 <devinedevops@gmail.com>
2023-01-24 20:45:57 +0200

    done

--- exercise19/ex19.2.js
+++ exercise19/ex19.2.js
@@ -1,41 +1,47 @@
+'use strict'
 const users = [
     {
-    id: 167464,
-    firstName: "Jimmy",
-    lastName: "Arnold",
-    email: "jimmya@gmail.com",
+        id: 167464,
+        firstName: "Jimmy",
+        lastName: "Arnold",
+        email: "jimmya@gmail.com",
     },
     {
-    id: 578447,
-    firstName: "Martha",
-    lastName: "Goldman",
-    email: "gold@hotmail.com",
+        id: 578447,
+        firstName: "Martha",
+        lastName: "Goldman",
+        email: "gold@hotmail.com",
     },
     {
-    id: 864578,
-    firstName: "Tim",
-    lastName: "Burton",
-    email: "timmy.hotmail.com",
+        id: 864578,
+        firstName: "Tim",
+        lastName: "Burton",
+        email: "timmy.hotmail.com",
     },
-    ];
-
+];
 // Instructions
 // You got an array of objects of users from your database
 //1. Loop over the array with the forEach method and
 // dynamically create an ordered list of the first and last
 // names of the objects.
-const newOl = document.createElement('ol');
-const body = document.querySelector('body');
+const newOl = document.createElement("ol");
+const body = document.querySelector("body");
 body.appendChild(newOl);
-newOl.innerText = 'ordered List'
+newOl.innerText = "ordered List";
 users.forEach((usersData) => {
-    const olNameAndLastName = usersData.firstName + ' ' + usersData.lastName;
-    const newLi = document.createElement('li');
+    const olNameAndLastName = usersData.firstName + " " + usersData.lastName;
+    const newLi = document.createElement("li");
     newOl.appendChild(newLi);
     newLi.innerText = olNameAndLastName;
-})
+});
 // 2. Remove the bullet points of the ordered list with
 // JavaScript.
-
+const ol = document.querySelector("ol");
+ol.style.listStyleType = "none";
 // 3. Create a custom data attribute called “data-id” and attach
 // the id value to each li
+users.forEach((userData) => {
+    const userId = document.querySelector("li");
+    userId.setAttribute("data-id", userData.id);
+    newOl.appendChild(userId);
+});
