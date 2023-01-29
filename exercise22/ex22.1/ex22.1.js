8975312e315472b75a42bc9f2c010110590670c1
Nita1001 <devinedevops@gmail.com>
2023-01-29 11:20:07 +0200

    Initial commit

---
+++ exercise22/ex22.1/ex22.1.js
@@ -0,0 +1,7 @@
+// Create a webpage that has a form (asks the user for its name,
+// age, and email address) and when the user clicks submit, open
+// a text window that writes all the information and asks for his
+// confirmation. If he clicks on the confirm button, tell him
+// “congratulations you successfully sent this form” if he clicks on
+// change information, give him the possibility to change the
+// information, and ask again for his confirmation.
