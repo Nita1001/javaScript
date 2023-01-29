d8bed1eb5309fce97770879f461cbce1e72b073a
Nita1001 <devinedevops@gmail.com>
2023-01-29 21:47:03 +0200

    halfway through

--- exercise22/ex22.1/ex22.1.js
+++ exercise22/ex22.1/ex22.1.js
@@ -5,3 +5,33 @@
 // “congratulations you successfully sent this form” if he clicks on
 // change information, give him the possibility to change the
 // information, and ask again for his confirmation.
+const usName = document.querySelector('#users-name');
+const usAge = document.querySelector('#users-age');
+const usEmail = document.querySelector('#users-email');
+const form = document.querySelector('#signup-form');
+const submitBtn = document.querySelector('#submit-form');
+const confirmBtn = document.querySelector('#confirm-form');
+const changeBtn = document.querySelector('#incorrect');
+formData = {};
+
+/// Functions
+const submit = () => {
+    alert('submit');
+};
+const confirm = () => {
+
+};
+const changeForm = () => {
+    
+};
+
+// EventListeners
+form.addEventListener('submit',  (ev) => {
+    console.log('Name', usName.value);
+    console.log('Age', usAge.value);
+    console.log('email', usEmail.value);
+    ev.preventDefault();
+})
+submitBtn.addEventListener('click', submit());
+changeBtn.addEventListener('click', changeForm());
+confirmBtn.addEventListener('click', confirm());
