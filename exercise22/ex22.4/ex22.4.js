ae27da96a83332445272b501e25bd951acc3b82d
Nita1001 <devinedevops@gmail.com>
2023-01-30 13:49:55 +0200

    Initial commit

---
+++ exercise22/ex22.4/ex22.4.js
@@ -0,0 +1,13 @@
+// Things to think about
+// ● We need to update a player’s position by moving the
+// ‘active’ class from one ‘td’ to the next one. Of course,
+// there are other solutions to implement this game. Using a
+// table and a CSS class is one option. But we will stick to
+// this one.
+// ● We need a way for JavaScript to update the board state.
+// Create simple JavaScript functions that update a player’s
+// position.
+// ● To move the car do not use the click event. Use the
+// ‘keyup’ event.
+// ● Find a way to announce the winner of the race.
+// ● Find a way to restart the game.
