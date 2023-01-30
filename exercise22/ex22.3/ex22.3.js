cfcc32a2ac328fe6a4a23413ae0cd6eb83980e4d
Nita1001 <devinedevops@gmail.com>
2023-01-30 12:47:22 +0200

    done

--- exercise22/ex22.3/ex22.3.js
+++ exercise22/ex22.3/ex22.3.js
@@ -1,26 +1,57 @@
 // Guess My Letter
-//generating random lowerCase letter 
-'use strict'
-const randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
-const inputLetter = document.querySelector('#keys-guessed');
-const correctGuess = document.querySelector('#nice-guess');
-const wrongGuess = document.querySelector('#wrong-guess');
-inputLetter.addEventListener('keypress', (ev) => {
-    console.log(ev.key);
-    console.log(randomLetter);
-    if(ev.key === randomLetter)
-    {   
-        wrongGuess.classList.add('hide');
-        correctGuess.classList.remove('hide');
-        inputLetter.addEventListener('keydown', (ev) =>{
-            ev.preventDefault();
-            return false; 
-        });
-    } else{
-        wrongGuess.classList.remove('hide');
+//generating random lowerCase letter
+"use strict";
+const inputLetter = document.querySelector("#keys-guessed");
+const correctGuess = document.querySelector("#nice-guess");
+const wrongGuess = document.querySelector("#wrong-guess");
+const wouldYou = document.querySelector("#would-you");
+const playAgainBtn = document.querySelector("#play-again");
+const showCorrectLetter = document.querySelector('#show-letter');
+const hideQuestion = document.querySelector('#question');
+let randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+let endGame = false;
+
+function newGame() {
+    randomLetter = String.fromCharCode(97 + Math.floor(Math.random() * 26));
+    wouldYou.classList.add("hide");
+    playAgainBtn.classList.add("hide");
+    correctGuess.classList.add("hide");
+    hideQuestion.classList.remove('hide');
+    showCorrectLetter.classList.add('hide');
+    inputLetter.value = "";
+    endGame = false;
+}
+function showLetter(){
+    showCorrectLetter.innerText = randomLetter;
+    hideQuestion.classList.add('hide');
+    showCorrectLetter.classList.remove('hide');
+}
+function correct(){
+    showLetter();
+    setTimeout(() => {
+        wouldYou.classList.remove("hide");
+        playAgainBtn.classList.remove("hide");
+    }, 1000);
+    wrongGuess.classList.add("hide");
+    correctGuess.classList.remove("hide");
+    endGame = true;
+}
+
+inputLetter.addEventListener("keypress", (ev) => {
+    if(!endGame)
+    {
+        console.log(randomLetter);
+        if (ev.key === randomLetter) {
+            correct();
+        } else {
+            wrongGuess.classList.remove("hide");
+        }
+    } else {
+        ev.preventDefault();
+        return false;
     }
-})
+});
 
-
-// after reset game 
-//         correctGuess.classList.add('hide');
+playAgainBtn.addEventListener("click", () => {
+    newGame();
+});
