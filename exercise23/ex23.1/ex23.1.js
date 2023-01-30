b36aed831581befad32c032c622442b0f5ef8414
Nita1001 <devinedevops@gmail.com>
2023-01-30 13:56:13 +0200

    Initial commit

---
+++ exercise23/ex23.1/ex23.1.js
@@ -0,0 +1,32 @@
+// Answer the following questions:
+// Question 1:
+// In your own words what will this point to and why?
+// (Note this is the global scope)
+console.log(this);
+// Question 2:
+// a. In your own words what will this point to and why?
+// b. How can you fix this code?
+const myObj = {
+name: "Timmy",
+greet: () => {
+console.log(`Hello ${this.name}`);
+},
+};
+myObj.greet();
+Question 3:
+In your own words what will this point to and why?
+const myFuncDec = function () {
+console.log(this);
+};
+// Question 4:
+// In your own words what will this point to and why?
+const myFuncArrow = () => {
+console.log(this);
+};
+myFuncArrow();
+// Question 5:
+// a. In your own words, what will this point to and why?
+// b. How can you fix this code?
+document.querySelector(".element").addEventListener(() => {
+console.log(this);
+});
---
+++ exercise23/ex23.2/ex23.2.js
@@ -0,0 +1,13 @@
+// Your Task: Edit the storm object. Invoke the printSuperPower
+// function using the storm object as the context of the function.
+// So your output should be: my superpower is flying.
+// Well, Storm also controls the weather, so, whatever you
+// choose!
+// You cannot change the printSuperPower function
+const storm = {
+// add code here
+}
+function printSuperPower() {
+console.log("my superpower is " +
+this.superPower);
+}
