b2efc226fa07fff1bf5157f093d8aea51acd5cd0
Nita1001 <devinedevops@gmail.com>
2023-01-31 09:48:42 +0200

    done

--- exercise21/ex21.3/ex21.3.js
+++ exercise21/ex21.3/ex21.3.js
@@ -4,3 +4,15 @@
 // characters before clicking the button. If he didn’t, please
 // give him the appropriate message. (using alert is
 // forbidden)
+const textArea = document.querySelector('textarea');
+const submitBtn = document.querySelector('button');
+const message = document.querySelector('h1');
+textArea.setAttribute('minlength', '100');
+textArea.setAttribute('required', '');
+submitBtn.addEventListener('click', () =>{
+    if(textArea.value.length < 100){
+        message.classList.remove('hide');
+    } else{
+        message.classList.add('hide');
+    }
+})
