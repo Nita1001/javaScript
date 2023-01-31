aa1aee760d125688fd746e5cda3aed9849acd4e0
Nita1001 <devinedevops@gmail.com>
2023-01-31 10:32:46 +0200

    done

--- exercise21/ex21.5/ex21.5.js
+++ exercise21/ex21.5/ex21.5.js
@@ -3,3 +3,19 @@
 // to make them appear on the screen. If the user enters an input
 // that is not a number show him an error message.
 // Ninja - Remove all smiles when entering new ones
+const input = document.querySelector('input');
+const submitBtn = document.querySelector('button');
+const container = document.querySelector('.smiles');
+const smiley =  document.querySelector('img');
+
+const createSmileys = () => {
+    container.innerHTML = '';
+    for(let i = 0; i < input.value; i++)
+    {
+        const newSome = document.createElement('img');
+        newSome.setAttribute('src', './images/smilingSkullMelting.PNG')
+        container.appendChild(newSome);
+    }
+}
+
+submitBtn.addEventListener('click', createSmileys)
