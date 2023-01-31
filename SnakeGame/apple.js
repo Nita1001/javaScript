d489d866f7d28370e01fc74a7bb13630a451a029
Nita1001 <devinedevops@gmail.com>
2023-01-31 19:52:48 +0200

    snakes food

---
+++ SnakeGame/apple.js
@@ -0,0 +1,17 @@
+let apple = {x: 10, y: 1};
+const EXPANSION_RATE = 2;
+
+export function update() {
+  if(onSnake(apple)) {
+    expandSnake(EXPANSION_RATE);
+    apple = {x: 20, y: 10};
+  }
+}
+
+export function draw(gameBoard) {
+        const appleElement = document.createElement('div');
+        appleElement.style.gridRowStart = apple.y;
+        appleElement.style.gridColumnStart = apple.x;
+        appleElement.classList.add('apple');
+        gameBoard.appendChild(appleElement);
+}
