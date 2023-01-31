92ddb8873dccd9c1651957b6e7958dca2d864ac8
Nita1001 <devinedevops@gmail.com>
2023-01-31 11:19:37 +0200

    done

--- exercise24/ex24.1/ex24.1.js
+++ exercise24/ex24.1/ex24.1.js
@@ -4,20 +4,20 @@
 // isSquare that prints true if a Square object has equal edges
 // and false if they are unequal.
 // Here is the Square function:
-function Square(a, b, c, d){
-this.a = a;
-this.b = b;
-this.c = c;
-this.d = d;
+function Square(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
 }
 
 Square.prototype.isSquare = function () {
-    if(this.a === this.b && this.b === this.c && this.c === this.d){
-        console.log('true!');
-    } else{
-        console.log('false.');
+    if (this.a === this.b && this.b === this.c && this.c === this.d) {
+        console.log("true!");
+    } else {
+        console.log("false.");
     }
-}
+};
 
-const s1 = new Square(4,4,4,4);
+const s1 = new Square(4, 4, 4, 4);
 s1.isSquare();
