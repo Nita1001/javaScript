14b6ea19d5ad5a5b56c8dd5361f9f1b4c3b0ae20
Nita1001 <devinedevops@gmail.com>
2023-01-31 09:07:19 +0200

    done

--- exercise21/ex21.4/ex21.4.js
+++ exercise21/ex21.4/ex21.4.js
@@ -1,3 +1,16 @@
 // Create a webpage that has an image of a lightbulb.
 // When the user clicks the lightbulb, it should be turned
 // on, clicking it again will turn it off.
+const lightBulb = document.querySelector('#lights');
+let lightOn = false;
+
+lightBulb.addEventListener('click', () => {
+    if(!lightOn)
+    {
+        lightBulb.src = './images/bulb-on.jpg'
+        lightOn = true;
+    } else{
+        lightBulb.src = './images/bulb-off.jpg';
+        lightOn = false;
+    }
+});
