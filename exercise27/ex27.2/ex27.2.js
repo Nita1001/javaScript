833f80b54be8c7413d56091e3f1f52036651bf74
Nita1001 <devinedevops@gmail.com>
2023-02-06 14:24:31 +0200

    Initial commit

---
+++ exercise27/ex27.2/ex27.2.js
@@ -0,0 +1,8 @@
+// Write two functions that use Promises that you can chain.
+// The first function, makeAllCaps(), will take in an array of words
+// and capitalize them, and then the second function, sortWords(),
+// will sort the words in alphabetical order. If the array contains
+// anything but strings, it should throw an error.
+// Call the functions once with an array of words and call it once
+// with an array that includes at least one item that is not a word.
+// Print the resolve and reject in a .then, .catch.
