b7e4657d24cc0d751971e227b10cb7e708abacd1
Nita1001 <devinedevops@gmail.com>
2023-02-07 14:39:02 +0200

    done

--- exercise28/ex28.1.js
+++ exercise28/ex28.1.js
@@ -1,2 +1,20 @@
-// Create a button that will fetch the joke of the day.
-// Display to the screen the joke title and the joke itself
+const getTextBtn = document.querySelector('#getJoke');
+getTextBtn.addEventListener('click', getText);
+
+function getText(){
+    fetch('https://api.jokes.one/jod')
+    .then((res) => res.json())
+    .then((data) => {
+        let output ='';
+        const gotJoke = data.contents.jokes[0].joke;
+        console.log(gotJoke.title);
+        output = `
+        <div>
+            <h3>${gotJoke.title}</h3>
+            <p>${gotJoke.text}</p>
+        </div>
+        `;
+        document.querySelector('#output').innerHTML = output;
+    })
+    .catch((err) => console.log(err));
+}
