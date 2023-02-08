639d166d1118e464f04618e317ae4a1523d4c339
Nita1001 <devinedevops@gmail.com>
2023-02-09 00:01:44 +0200

    done with fetch - only 1 Person

--- exercise31/ex31.1.js
+++ exercise31/ex31.1.js
@@ -1,31 +1,17 @@
-let bigData = [];
-
-
-function people(){
-
-    for (let i = 1; i <= 10; i++) {
-        console.log(bigData);
-        data(i);
-       
+async function data(){
+    try{
+        const res = await fetch("https://swapi.dev/api/people/1/");
+        const data = await res.json();
+        const res2 = await fetch("https://swapi.dev/api/planets/1/");
+        const homePlanet = await res2.json();
+        const obj = {};
+        obj['Name'] = data.name;
+        obj['Height'] = data.height;
+        obj['Hair'] = data.hair_color;
+        obj['Planet'] = {'Name': homePlanet.name, 'Population': homePlanet.population};
+    } catch(err){
+        console.log('Error status:', err);
     }
-
-}
-
-async function data(i) {
-        try {
-            const res = await fetch(`https://swapi.dev/api/people/${i}`);
-            const data = await res.json();
-            const res2 = await fetch(`https://swapi.dev/api/planets/${i}`);
-            const homePlanet = await res2.json();
-            const obj = {};
-            obj['Name'] = data.name;
-            obj['Height'] = data.height;
-            obj['Hair'] = data.hair_color;
-            obj['Planet'] = { 'Name': homePlanet.name, 'Population': homePlanet.population };
-            bigData.push(obj);
-        } catch (err) {
-            console.log('Error status:', err);
-        }
 }
 
 data();
