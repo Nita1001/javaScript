898ff9c59d7c5020aa770cc5a4dc4beacc0f5423
Nita1001 <devinedevops@gmail.com>
2023-02-09 08:43:14 +0200

    done

--- exercise31/ex31.1.js
+++ exercise31/ex31.1.js
@@ -1,17 +1,29 @@
-async function data(){
-    try{
-        const res = await fetch("https://swapi.dev/api/people/1/");
-        const data = await res.json();
-        const res2 = await fetch("https://swapi.dev/api/planets/1/");
-        const homePlanet = await res2.json();
-        const obj = {};
-        obj['Name'] = data.name;
-        obj['Height'] = data.height;
-        obj['Hair'] = data.hair_color;
-        obj['Planet'] = {'Name': homePlanet.name, 'Population': homePlanet.population};
-    } catch(err){
+
+let bigData = [];
+
+async function data() {
+    try {
+        for (let i = 1; i <= 10; i++) {
+            const res = await fetch(`https://swapi.dev/api/people/${i}/`);
+            if (res.ok) {
+                const data = await res.json();
+                const res2 = await fetch(`https://swapi.dev/api/planets/${i}/`);
+                const homePlanet = await res2.json();
+                const obj = {};
+                obj['Name'] = data.name;
+                obj['Height'] = data.height;
+                obj['Hair'] = data.hair_color;
+                obj['Planet'] = { 'Name': homePlanet.name, 'Population': homePlanet.population };
+                bigData.push(obj);
+            } else {
+                throw new Error(res.status, res2.status)
+            }
+        }
+
+    } catch (err) {
         console.log('Error status:', err);
     }
 }
 
+console.log(bigData);
 data();
